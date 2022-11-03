157c84d7a924a2b697019989244878d9027c25d2
Gabriel Gazola Milan <gabriel.gazola@poli.ufrj.br>
2022-11-03 12:54:24 -0300

    feat: add persistent logging

--- api_dados_rio/settings/base.py
+++ api_dados_rio/settings/base.py
@@ -156,6 +156,42 @@ REST_FRAMEWORK = {
     "PAGE_SIZE": 20,
 }
 
+# Logging
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "handlers": {
+        # Include the default Django email handler for errors
+        # This is what you'd get without configuring logging at all.
+        "mail_admins": {
+            "class": "django.utils.log.AdminEmailHandler",
+            "level": "ERROR",
+            # But the emails are plain text by default - HTML is nicer
+            "include_html": True,
+        },
+        # Log to a text file that can be rotated by logrotate
+        "logfile": {
+            "class": "logging.handlers.WatchedFileHandler",
+            "filename": "/var/log/django/api_dados_rio.log",
+        },
+    },
+    "loggers": {
+        # Again, default Django configuration to email unhandled exceptions
+        "django.request": {
+            "handlers": ["mail_admins"],
+            "level": "ERROR",
+            "propagate": True,
+        },
+        # Might as well log any errors anywhere else in Django
+        "django": {
+            "handlers": ["logfile"],
+            "level": "ERROR",
+            "propagate": False,
+        },
+    },
+}
+
+
 # Comando API configuration
 API_USERNAME = getenv("API_USERNAME", "")
 API_PASSWORD = getenv("API_PASSWORD", "")
