639b6c98764d4e610d4bf480b989b4d11689a496
patriciacatandi <patriciabcatandi@gmail.com>
2024-02-28 10:03:48 -0300

    testing class heritage

--- api_dados_rio/v2/clima_pluviometro/views.py
+++ api_dados_rio/v2/clima_pluviometro/views.py
@@ -37,14 +37,16 @@ from rest_framework_tracking.mixins import LoggingMixin
     ),
 )
 class Last15MinRainView(LoggingMixin, ViewSet):
+    def __init__(self):
+        self.data_key = "data_last_15min_rain"
+
     def list(self, request):
-        data_key = "data_last_15min_rain"
         try:
             redis_url = getenv("REDIS_URL")
             assert redis_url is not None
             redis = RedisPal.from_url(redis_url)
             # Get data and set cache
-            data = redis.get(data_key)
+            data = redis.get(self.data_key)
             assert data is not None
             assert isinstance(data, list)
             assert len(data) > 0
@@ -72,13 +74,15 @@ class Last15MinRainView(LoggingMixin, ViewSet):
     ),
 )
 class LastUpdateRainView(LoggingMixin, ViewSet):
+    def __init__(self):
+        self.last_update_key = "data_last_15min_rain_update"
+
     def list(self, request):
-        last_update_key = "data_last_15min_rain_update"
         try:
             redis_url = getenv("REDIS_URL")
             assert redis_url is not None
             redis = RedisPal.from_url(redis_url)
-            data = redis.get(last_update_key)
+            data = redis.get(self.last_update_key)
             assert data is not None
             assert isinstance(data, list)
             assert len(data) > 0
@@ -92,6 +96,7 @@ class LastUpdateRainView(LoggingMixin, ViewSet):
                 {"error": "Something went wrong. Try again later."},
                 status=500,
             )
+
 
 # Views for last 120 min
 @method_decorator(
@@ -120,24 +125,10 @@ class LastUpdateRainView(LoggingMixin, ViewSet):
         """,
     ),
 )
-class Last120MinRainView(LoggingMixin, ViewSet):
-    def list(self, request):
-        data_key = "data_chuva_passado_alertario"
-        try:
-            redis_url = getenv("REDIS_URL")
-            assert redis_url is not None
-            redis = RedisPal.from_url(redis_url)
-            # Get data and set cache
-            data = redis.get(data_key)
-            assert data is not None
-            assert isinstance(data, list)
-            assert len(data) > 0
-            return Response(data)
-        except Exception:
-            return Response(
-                {"error": "Something went wrong. Try again later."},
-                status=500,
-            )
+class Last120MinRainView(Last15MinRainView):
+    def __init__(self):
+        super().__init__()
+        self.data_key = "data_chuva_passado_alertario"
 
 
 @method_decorator(
@@ -155,24 +146,7 @@ class Last120MinRainView(LoggingMixin, ViewSet):
         """,
     ),
 )
-class LastUpdate120MinRainView(LoggingMixin, ViewSet):
-    def list(self, request):
-        last_update_key = "data_update_chuva_passado_alertario"
-        try:
-            redis_url = getenv("REDIS_URL")
-            assert redis_url is not None
-            redis = RedisPal.from_url(redis_url)
-            data = redis.get(last_update_key)
-            assert data is not None
-            assert isinstance(data, list)
-            assert len(data) > 0
-            result = data[0]
-            assert "last_update" in result
-            last_update = result["last_update"]
-            last_update_str = last_update.strftime("%d/%m/%Y %H:%M:%S")
-            return Response(last_update_str)
-        except Exception:
-            return Response(
-                {"error": "Something went wrong. Try again later."},
-                status=500,
-            )
+class LastUpdate120MinRainView(LastUpdateRainView):
+    def __init__(self):
+        super().__init__()
+        self.last_update_key = "data_update_chuva_passado_alertario"
